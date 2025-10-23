f8cdd42ca8faedb08b4f4ad73449223f0d6ecc16
sachintha Nirmal <s.nirmal.sm@gmail.com>
2025-10-23 14:32:17 +0530

    Add ExampleComponent and ProtectedRoute for basic routing and demonstration

---
+++ frontend/src/components/ExampleComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            <h1>This is an Example Component</h1>
+            <p>This component serves as a demonstration of a functional React component.</p>
+        </div>
+    );
+};
+
+export default ExampleComponent;
